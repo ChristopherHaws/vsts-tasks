2872a7a15a290a1b670c8d3163d665166ac9450d
ChristopherHaws <cribs2@gmail.com>
2016-02-17 15:52:30 -0800

    Gulp NPM and TSD

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,48 @@
 var gulp = require('gulp');
 var del = require('del');
 var typescript = require('gulp-typescript');
+var install = require('gulp-install');
+var tsd = require('gulp-tsd');
 var project = typescript.createProject("tsconfig.json");
 
 // gulp.task('clean', function (cb) {
 //     del([_buildRoot, _pkgRoot, _wkRoot, _oldPkg], cb);
 // });
 
+gulp.task('install-task-dependencies', function (cb) {
+    return gulp
+		.src(['./Tasks/**/package.json'])
+        .pipe(install());
+});
+
+gulp.task('tsd', function (cb) {
+    return gulp
+		.src(['./Tasks/**/tsd.json'])
+        .pipe(tsd());
+});
+
 gulp.task('build', function (cb) {
     return gulp
 		.src([
 			'./typings/**/*.d.ts',
 			'./Tasks/**/*.ts'
-		], { base: "." })
+		])
         .pipe(typescript(project))
-		.pipe(gulp.dest('.'));
+		.pipe(gulp.dest('./_build/'));
 });
 
-gulp.task('default', ['build']);
+gulp.task('copy-task-files', function (cb) {
+    return gulp
+		.src([
+			'./Tasks/**/task.json',
+			'./Tasks/**/icon.png',
+			'./Tasks/**/*.ps1'
+		])
+		.pipe(gulp.dest('./_build/'));
+});
+
+gulp.task('default', [
+	'install-task-dependencies',
+	'build',
+	'copy-task-files'
+]);
