3a81d99cc160607b433b37c0cbc8c8034561b9ef
ChristopherHaws <cribs2@gmail.com>
2016-02-17 19:21:54 -0800

    Started implementing vsts-iis

--- Shared/vsts-iis/iis-vsts.js
+++ Shared/vsts-iis/iis-vsts.js
@@ -1,1 +1,21 @@
-
+var os = require("os");
+var vsts = require("vsts-task-lib/task");
+var IIS = (function () {
+    function IIS() {
+        if (os.arch() === "x64") {
+            this.toolRunner = vsts.createToolRunner(process.env["windir"] + "\\syswow64\\inetsrv\\appcmd.exe");
+        }
+        else {
+            this.toolRunner = vsts.createToolRunner(process.env["windir"] + "\\system32\\inetsrv\\appcmd.exe");
+        }
+    }
+    IIS.prototype.createSite = function (options) {
+        vsts.debug("Creating site...");
+        this.toolRunner.arg("add site");
+        this.toolRunner.arg(["/name:", options.name]);
+        this.toolRunner.arg(["/bindings:", options.bindings]);
+        this.toolRunner.argIf(options.path, ["/physicalPath:", options.path]);
+        var results = this.toolRunner.execSync();
+    };
+    return IIS;
+})();
--- Shared/vsts-iis/iis-vsts.ts
+++ Shared/vsts-iis/iis-vsts.ts
@@ -1,1 +1,30 @@
+import * as os from "os";
 import * as vsts from "vsts-task-lib/task";
+import * as toolRunner from "vsts-task-lib/toolrunner";
+
+interface IisSite {
+	name: string;
+	bindings: string;
+	path: string;
+}
+
+class IIS {
+	private toolRunner: toolRunner.ToolRunner;
+	
+	constructor() {
+		if (os.arch() === "x64") {
+			this.toolRunner = vsts.createToolRunner(process.env["windir"] + "\\syswow64\\inetsrv\\appcmd.exe");
+		} else {
+			this.toolRunner = vsts.createToolRunner(process.env["windir"] + "\\system32\\inetsrv\\appcmd.exe");
+		}
+	}
+	
+	public createSite(options: IisSite): void {
+		vsts.debug("Creating site...");
+		this.toolRunner.arg("add site");
+		this.toolRunner.arg(["/name:", options.name]);
+		this.toolRunner.arg(["/bindings:", options.bindings]);
+		this.toolRunner.argIf(options.path, ["/physicalPath:", options.path]);
+		var results = this.toolRunner.execSync();
+	}
+}
