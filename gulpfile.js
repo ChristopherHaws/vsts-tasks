ed03a890d2f569d73f4a33f4c9bdf293820cb114
ChristopherHaws <cribs2@gmail.com>
2016-02-17 18:30:04 -0800

    Setup typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,35 @@
 var gulp = require('gulp');
+var typescript = require('gulp-typescript');
 var install = require('gulp-install');
 var debug = require('gulp-debug');
+var project = typescript.createProject("tsconfig.json")
 
-gulp.task('task-npm', function (cb) {
-    return gulp
-		.src(['./Tasks/**/package.json', '!**/node_modules/**'])
+gulp.task('project-npm', function (cb) {
+	return gulp
+		.src(['package.json'])
+		.pipe(install());
+});
+
+gulp.task('task-npm', ['project-npm'], function (cb) {
+	return gulp
+		.src([
+			'./Shared/**/package.json',
+			'./Tasks/**/package.json',
+			'!**/node_modules/**'
+		])
         //.pipe(debug());
 		.pipe(install());
 });
 
-gulp.task('default', ['task-npm']);
+gulp.task('typescript', ['task-npm'], function (cb) {
+    return gulp
+        .src([
+            './typings/**/*.d.ts',
+            './Shared/**/*.ts',
+            './Tasks/**/*.ts'
+        ], { base: "." })
+        .pipe(typescript(project))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('default', ['project-npm', 'task-npm', 'typescript']);
