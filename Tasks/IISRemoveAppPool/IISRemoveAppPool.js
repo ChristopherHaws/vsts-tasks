c0a0c814fdc4ecfb46eb9859b9191b1545aeb4cd
ChristopherHaws <cribs2@gmail.com>
2016-02-19 21:17:14 -0800

    Updated to latest version of vsts-iis

--- Tasks/IISRemoveAppPool/IISRemoveAppPool.js
+++ Tasks/IISRemoveAppPool/IISRemoveAppPool.js
@@ -1,3 +1,11 @@
+//import * as runner from "vsts-task-lib/toolrunner";
+var iis = require("vsts-iis");
+iis.Sites.addSync({
+    name: "Foo",
+    protocol: "https",
+    host: "*",
+    port: 443
+});
 // 
 // iis.createSiteSync({
 // 	name: "Foo",
